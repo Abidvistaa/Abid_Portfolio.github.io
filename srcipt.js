7fc6aff54ee858c9b7d08de8d60c623614ea52e3
Abid Hassan <abidhassandiba@gmail.com>
2021-08-18 11:15:56 +0600

    Section: s1

--- srcipt.js
+++ srcipt.js
@@ -46,6 +46,7 @@ $(document).ready(function(){
     $('.carousel').owlCarousel({
         margin:20,
         loop:true,
+        autoplay:true,
         autoplayTimeout:2000,
         autoplayHoverPause:true,
         responsive:{
